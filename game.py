5854dc2d0b12e46a7222dc583ebafea2edd252be
jmilcent <johan.milcent@gmail.com>
2018-11-27 14:50:37 +0100

    first commit for longest world game

---
+++ game.py
@@ -0,0 +1,29 @@
+"""
+comment for import
+"""
+import random
+import string
+
+
+class Game:
+    """
+    class Game
+    """
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, opt_str):
+        """
+        check if input data is valid
+        """
+        if not opt_str:
+            return False
+        letters = self.grid.copy()
+        for letter in opt_str:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return False
